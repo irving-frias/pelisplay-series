1c7d740ce60271111e42fbf315c17641cfb6d6de
Erick G. Islas-Osuna <erickisos653@gmail.com>
2022-03-21 13:25:41 -0600

    add project definition and module dir

--- src/pelisplay-series/__main__.py
+++ src/pelisplay-series/__main__.py
@@ -1,6 +1,18 @@
 import os
+from argparse import ArgumentParser
+from urllib.parse import urlparse
 
 import requests
+from tqdm import tqdm
+
+
+def extract_name(url: str, domain: str = 'www.pelisplay.co') -> str:
+    """Devuelve el nombre de la serie si el dominio es el apropiado"""
+    parsed_url = urlparse(url)
+    last_element = parsed_url.path.split('/')[-1]
+    if parsed_url.hostname == domain:
+        return last_element
+    raise ValueError('La URL no es válida')
 
 
 def ensure_path(base_path: str, prefix: str, path_id: int):
@@ -17,15 +29,13 @@ def request_data(url: str):
     return response.json()['data']
 
 
-def main():
-    parent_dir = os.getcwd()
-    serie = input('Ingrese el nombre de la serie: ')
+def main(url: str, path: str):
+    serie = extract_name(url)
     url_base = f'https://www.pelisplay.co/api/serie/{serie}/'
     data = request_data(url_base)
+    print(data)
     temporadas = int(data["n_temporadas"])
 
-    nombre_carpeta = input('Ingrese el nombre de la carpeta: ')
-    path = os.path.join(parent_dir, nombre_carpeta)
     if not (os.path.exists(path)):
         os.mkdir(path)
 
@@ -33,11 +43,14 @@ def main():
         path_temp = ensure_path(path, 'temporada', temp)
 
         season_data = request_data(f'{url_base}temporada-{temp}')
+        print(season_data)
         episodios = int(season_data["n_episodios"])
+        print(f'Temporada: {temp}')
 
-        for episode in range(episodios):
+        for episode in tqdm(range(episodios)):
             _epsiode_url = season_data["episodios"][episode]["url"]
             _episode_data = request_data(_epsiode_url)
+            print(_episode_data)
             _enlaces = _episode_data["enlaces"]["online"]
             episode_path = ensure_path(path_temp, 'episodio', episode + 1)
 
@@ -46,7 +59,9 @@ def main():
                 language = optional_chapters['idioma']
                 __chapter__url = __chapter["url"]
                 filename = os.path.join(
-                    episode_path, f'episodio-{episode + 1}-{language}-{index}.txt')
+                    episode_path,
+                    f'episodio-{episode + 1}-{language}-{index}.txt',
+                )
 
                 if os.path.isfile(filename):
                     os.remove(filename)
@@ -56,4 +71,22 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    parser = ArgumentParser(
+        'Este programa te permite descargar series desde pelisplay.co'
+    )
+
+    parser.add_argument(
+        '-u', '--url', type=str, required=True, help='URL de la serie'
+    )
+    parser.add_argument(
+        '-o',
+        '--output',
+        type=str,
+        required=False,
+        help='Directorio en el que deseas almacenar la serie',
+        default='pelisplay',
+    )
+
+    args = parser.parse_args()
+
+    main(args.url, args.output)
